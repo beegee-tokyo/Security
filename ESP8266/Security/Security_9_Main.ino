d92de07cf1954cdd805431dad0170697c0b9cbd2
beegee-tokyo <bernd@giesecke.tk>
2015-12-12 22:29:54 +0800

    Added missing ESP8266 code

---
+++ ESP8266/Security/Security_9_Main.ino
@@ -0,0 +1,37 @@
+/**
+   loop()
+
+   Handling of GPIO and timer interrupts
+
+   @author Bernd Giesecke
+   @version 0.1 beta December 2, 2015.
+*/
+
+void loop() {
+  // Handle new PIR status if available
+  if (pirTriggered && alarmOn) {
+    pirTriggered = false;
+    alarmTimer.attach_ms(melodyTuneTime, playAlarmSound);
+    sendAlarm();
+  }
+
+  // Handle new time update request
+  if (timeUpdateTriggered) {
+    timeUpdateTriggered = false;
+    getTime();
+  }
+
+  // Handle new light update request
+  if (lightUpdateTriggered) {
+    lightUpdateTriggered = false;
+    getLight();
+  }
+
+  // Handle new client request on HTTP server if available
+  WiFiClient client = server.available();
+  if (client) {
+    sendLight(client);
+  }
+}
+
+
---
+++ ESP8266/Security/wifiAPinfo.h
@@ -0,0 +1,5 @@
+/** SSID of local WiFi network */
+const char* ssid = "Teresa&Bernd";
+/** Password for local WiFi network */
+const char* password = "teresa1963";
+
